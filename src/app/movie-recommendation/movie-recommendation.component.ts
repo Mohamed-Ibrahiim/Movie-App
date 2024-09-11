042ba5ac9c47b22c7a1cc639b696203e1283acab
“Mohamed <mohamedebraheem19246@gmail.com>
2024-09-11 20:54:54 +0300

    initial structure

---
+++ src/app/movie-recommendation/movie-recommendation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-recommendation',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-recommendation.component.html',
+  styleUrl: './movie-recommendation.component.css'
+})
+export class MovieRecommendationComponent {
+
+}
