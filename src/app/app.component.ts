330b4ccb403bcfe4e61f2d721af718a1b3adc1d1
“Mohamed <mohamedebraheem19246@gmail.com>
2024-09-13 15:41:57 +0300

    home header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { HeaderHomeComponent } from "./header-home/header-home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderHomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
